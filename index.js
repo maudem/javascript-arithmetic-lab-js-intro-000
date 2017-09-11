6773c422ba5e68bd2c7918fea93d4ca9ee35e00c
maude sos <mualth@gmail.com>
2017-09-11 01:17:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function add(a, b) {
-  
+  return a+b
 }
 
 function subtract() {
-  
+
 }
 
 function multiply() {
-  
+
 }
 
 function divide() {
-  
+
 }
 
 function inc() {
-  
+
 }
 
 function dec() {
-  
+
 }
 
 function makeint() {
-  
+
 }
